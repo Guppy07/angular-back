78cbca2426313c5eb6ad55acca6ea1045440f0fb
Nicolas <nicolas.jorge.munoz@gmail.com>
2024-02-22 16:51:03 -0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { CounterModule } from './counter/components/counter.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-
+import { CounterModule } from './counter/components/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule, DbzModule],
 })
 export class AppModule {}
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,13 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/dbz/components/list/list.component';
+import { AddCharacterComponent } from './components/dbz/components/add-character/add-character.component';
+
+@NgModule({
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
+  imports: [CommonModule],
+  exports: [MainPageComponent],
+})
+export class DbzModule {}
