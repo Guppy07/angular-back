25fe5d40dbc180eddf6364ab37a3ebc200d523c4
Nicolas <nicolas.jorge.munoz@gmail.com>
2024-02-23 13:26:11 -0300

    finalizacion seccion 5

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -8,5 +8,16 @@ import { Character } from './../interfaces/character.interface';
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  constructor(public dbzService: DbzService) {}
+  constructor(private dbzService: DbzService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.deleteCharacterById(id);
+  }
+  onNewCharacter(character: Character): void {
+    this.dbzService.addCharacter(character);
+  }
 }
