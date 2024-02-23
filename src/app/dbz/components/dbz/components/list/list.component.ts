2c041b9e0c4012c87074bd060d04a9ac5f0f50ce
Nicolas <nicolas.jorge.munoz@gmail.com>
2024-02-23 13:19:13 -0300

    clase 73

--- src/app/dbz/components/dbz/components/list/list.component.ts
+++ src/app/dbz/components/dbz/components/list/list.component.ts
@@ -16,10 +16,9 @@ export class ListComponent {
   ];
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index: number): void {
-    //TODO: Emitir el ID del personaje
-    this.onDelete.emit(index);
+  onDeleteCharacter(id?: string): void {
+    this.onDelete.emit(id);
   }
 }
