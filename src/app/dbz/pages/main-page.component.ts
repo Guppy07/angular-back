811b4ecc44d4caa55d37dd308ddc1becd6b4f082
Nicolas <nicolas.jorge.munoz@gmail.com>
2024-02-23 12:58:15 -0300

    clase 71

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,30 +1,12 @@
-import { Character } from './../interfaces/character.interface';
 import { Component, OnInit } from '@angular/core';
+
+import { DbzService } from './../services/dbz.service';
+import { Character } from './../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000,
-    },
-    {
-      name: 'Goku',
-      power: 9500,
-    },
-    {
-      name: 'Vegeta',
-      power: 7500,
-    },
-  ];
-
-  onNewCharacter(character: Character): void {
-    this.characters.push(character);
-  }
-  onDeleteCharacter(index: number) {
-    this.characters.splice(index, 1);
-  }
+  constructor(public dbzService: DbzService) {}
 }
